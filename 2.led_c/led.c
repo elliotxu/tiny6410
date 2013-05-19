c0aa24df5cb7366f7de26b44324dd6072dc509a7
lindabell <lindabell@163.com>
2013-05-19 15:22:03 +0800

    optimize code

--- 2.led_c/led.c
+++ 2.led_c/led.c
@@ -1,14 +1,12 @@
-
+//定义寄存器
+#define GPKCON0 (*((volatile unsigned long *)0x7F008800))
+#define GPKDAT 	(*((volatile unsigned long *)0x7F008808))
 int main()
-{
-	int i = 0;
-	//定义寄存器地址
-	volatile unsigned long *gpkcon = (volatile unsigned long *)0x7F008800;
-	volatile unsigned long *gpkdat = (volatile unsigned long *)0x7F008808;
-	
+{	
 	/* gpk7设为输出引脚 */
-	*gpkcon=0x10000000; //0001 0000 0000 0000 0000 0000 0000 00000=0x10000000
-	*gpkdat=0;			//输出为0
+	GPKCON0=GPKCON0&(~(0xf<<28))|(1<<28); //0001 0000 0000 0000 0000 0000 0000 00000=0x10000000
+	/*gpk7输出0*/
+	GPKDAT=GPKDAT & (~(1<<7));
 	return 0;
 }
 
